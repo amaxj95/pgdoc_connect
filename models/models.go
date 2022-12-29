47a259e158b61074c73af75b82f87096df860fcc
Aus10 <33111784+amaxj95@users.noreply.github.com>
2022-12-29 13:34:56 -0500

    init

---
+++ models/models.go
@@ -0,0 +1,9 @@
+package models
+
+import "gorm.io/gorm"
+
+type Crunchy_prereq struct {
+    gorm.Model
+    kubectl string `json:"kubectl version --short" gorm:"text;not null;default:null`
+    git   string    `json:"git --version" gorm:"text;not null;default:null"'
+}
