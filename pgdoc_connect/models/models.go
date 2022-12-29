347fd300728d7d2e9489322af626b3fe662b525d
Aus10 <amaxj95@gmail.com>
2022-12-29 13:57:14 -0500

    Update models.go

--- pgdoc_connect/models/models.go
+++ pgdoc_connect/models/models.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Crunchy_prereq struct {
     gorm.Model
-    kubectl string `json:"kubectl version --short" gorm:"text;not null;default:null`
+    kubectl string `json:"kubectl version --short" gorm:"text;not null;default:null"`
     git   string    `json:"git --version" gorm:"text;not null;default:null"'
 }
